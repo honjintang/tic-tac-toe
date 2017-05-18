57dacf3ffbf54a29446b9a63f60ad58c2105590a
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-05-18 13:07:12 +0100

    add default configuration for grid, grid spec tests pass

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,10 +1,17 @@
+require_relative 'cell'
+
 class Grid
 
   attr_reader :setup
 
-  def initialize(input)
-    @setup = input.fetch(:setup)
+  def initialize(input = {})
+    @setup = input.fetch(:setup, default)
   end
 
+  private
+
+  def default
+    Array.new(3) { Array.new(3) { Cell.new }}
+  end
 
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,12 +2,23 @@ require 'grid'
 
 describe Grid do
 
-  subject(:grid) { described_class.new(setup: :default_grid)}
+  subject(:grid) { described_class.new }
 
   context "#initialize" do
 
-    it "initializes the grid with a default grid" do
-      expect(grid.setup).to eq :default_grid
+    it "initializes the grid with a default configuration" do
+      expect{ Grid.new(setup: :any_config) }.to_not raise_error
     end
+
+    it "grid has 3 rows by default" do
+      expect(grid.setup.length).to eq(3)
+    end
+
+    it "each row contains 3 cells by default" do
+      grid.setup.each do |row|
+        expect(row.length).to eq(3)
+      end
+    end
+
   end
 end

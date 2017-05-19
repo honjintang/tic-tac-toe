9c0df3ab02d0e0da430e09ed3bc1c48d61b88449
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-05-19 15:35:34 +0100

    refactor

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,82 +3,82 @@ require 'grid'
 
 describe Game do
 
-  let(:player1) { double :player, mark: :X }
-  let(:player2) { double :player, mark: :O }
-  let(:grid) { Grid.new }
-  let(:input) { {player1: player1, player2: player2, grid: grid} }
-  subject(:game) { described_class.new(input) }
+    let(:player1) { double :player, mark: :X }
+    let(:player2) { double :player, mark: :O }
+    let(:grid) { Grid.new }
+    let(:input) { {player1: player1, player2: player2, grid: grid} }
+    subject(:game) { described_class.new(input) }
 
-  context "#initialize" do
+    context "#initialize" do
 
-    it "is initialized with player1 as current_player" do
-      expect(game.current_player).to eq player1
-    end
+      it "is initialized with player1 as current_player" do
+        expect(game.current_player).to eq player1
+      end
 
-    it "is initialized with a grid" do
-      expect(game.grid).to eq grid
-    end
+      it "is initialized with a grid" do
+        expect(game.grid).to eq grid
+      end
 
-  context "#claim" do
+    context "#claim" do
 
-    Test_cell = Struct.new(:value)
-    let(:x_cell) { Test_cell.new(:X) }
-    let(:o_cell) { Test_cell.new(:O) }
-    let(:empty_cell) { Test_cell.new(nil) }
+      Test_cell = Struct.new(:value)
+      let(:x_cell) { Test_cell.new(:X) }
+      let(:o_cell) { Test_cell.new(:O) }
+      let(:empty_cell) { Test_cell.new(nil) }
 
-    before do
-      test_config = [[empty_cell,x_cell, o_cell],
-                      [o_cell, o_cell, x_cell],
-                      [x_cell, o_cell, x_cell]]
-      grid = Grid.new(setup: test_config)
-      game = Game.new( {player1: player1, player2: player2, grid: grid} )
-    end
+      before do
+        test_config = [[empty_cell,x_cell, o_cell],
+                        [o_cell, o_cell, x_cell],
+                        [x_cell, o_cell, x_cell]]
+        grid = Grid.new(setup: test_config)
+        game = Game.new( {player1: player1, player2: player2, grid: grid} )
+      end
 
-    it "tells the grid to set a cell with the current player's mark" do
-      game.claim(0,0)
-      expect(game.grid.get_cell(0,0).value).to eq(:X)
-    end
+      it "tells the grid to set a cell with the current player's mark" do
+        game.claim(0,0)
+        expect(game.grid.get_cell(0,0).value).to eq(:X)
+      end
 
-    it "changes player's turn after a go" do
-      game.claim(0,0)
-      expect(game.current_player).to eq player2
-    end
+      it "changes player's turn after a go" do
+        game.claim(0,0)
+        expect(game.current_player).to eq player2
+      end
 
-    it "confirms the next player's turn" do
-      expect(game.claim(0,0)).to eq("Player O's turn")
-    end
+      it "confirms the next player's turn" do
+        expect(game.claim(0,0)).to eq("Player O's turn")
+      end
 
-    it "raises an error if player tries to claim a cell that is already taken" do
-      game.claim(0,0)
-      expect{ game.claim(0,0) }.to raise_error("This cell has already been taken")
-    end
+      it "raises an error if player tries to claim a cell that is already taken" do
+        game.claim(0,0)
+        expect{ game.claim(0,0) }.to raise_error("This cell has already been taken")
+      end
 
-    it "raises error if game is already over" do
-      test_config = [[empty_cell,x_cell, o_cell],
-                      [o_cell, o_cell, x_cell],
-                      [x_cell, x_cell, x_cell]]
-      grid = Grid.new(setup: test_config)
-      game = Game.new( {player1: player1, player2: player2, grid: grid} )
-      expect{ game.claim(0,0) }.to raise_error("The game is already over")
-    end
+      it "raises error if game is already over" do
+        test_config = [[empty_cell,x_cell, o_cell],
+                        [o_cell, o_cell, x_cell],
+                        [x_cell, x_cell, x_cell]]
+        grid = Grid.new(setup: test_config)
+        game = Game.new( {player1: player1, player2: player2, grid: grid} )
+        expect{ game.claim(0,0) }.to raise_error("The game is already over")
+      end
 
-     it "announces winner" do
-       test_config = [[empty_cell,x_cell, x_cell],
-                       [empty_cell, empty_cell, empty_cell],
-                       [o_cell, x_cell, o_cell]]
-       grid = Grid.new(setup: test_config)
-       game = Game.new( {player1: player1, player2: player2, grid: grid} )
-       expect(game.claim(0,0)).to eq("Player X wins!")
+       it "announces winner" do
+         test_config = [[empty_cell,x_cell, x_cell],
+                         [empty_cell, empty_cell, empty_cell],
+                         [o_cell, x_cell, o_cell]]
+         grid = Grid.new(setup: test_config)
+         game = Game.new( {player1: player1, player2: player2, grid: grid} )
+         expect(game.claim(0,0)).to eq("Player X wins!")
+       end
+
+       it "announces draw" do
+         test_config = [[empty_cell,o_cell, x_cell],
+                         [o_cell, o_cell, x_cell],
+                         [x_cell, x_cell, o_cell]]
+         grid = Grid.new(setup: test_config)
+         game = Game.new( {player1: player1, player2: player2, grid: grid} )
+         expect(game.claim(0,0)).to eq("Draw!")
+       end
      end
-
-     it "announces draw" do
-       test_config = [[empty_cell,o_cell, x_cell],
-                       [o_cell, o_cell, x_cell],
-                       [x_cell, x_cell, o_cell]]
-       grid = Grid.new(setup: test_config)
-       game = Game.new( {player1: player1, player2: player2, grid: grid} )
-       expect(game.claim(0,0)).to eq("Draw!")
-     end
-   end
-end
+  end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,99 +2,93 @@ require 'grid'
 
 describe Grid do
 
-  subject(:grid) { described_class.new }
-  Test_cell = Struct.new(:value)
+    subject(:grid) { described_class.new }
+    Test_cell = Struct.new(:value)
 
-  context "#initialize" do
+    context "#initialize" do
 
-    it "grid has 3 rows by default" do
-      expect(grid.setup.length).to eq(3)
-    end
+      it "grid has 3 rows by default" do
+        expect(grid.setup.length).to eq(3)
+      end
 
-    it "each row contains 3 cells by default" do
-      grid.setup.each do |row|
-        expect(row.length).to eq(3)
+      it "each row contains 3 cells by default" do
+        grid.setup.each do |row|
+          expect(row.length).to eq(3)
+        end
+      end
+
+    context "#get_cell" do
+
+      it "returns the cell at a given [x, y] coordinates" do
+        test_config = [[Test_cell.new(:X),nil,nil], [nil,nil,nil], [nil,nil,nil]]
+        grid = Grid.new({setup: test_config})
+        expect(grid.get_cell(0, 0).value).to eq(:X)
       end
     end
 
-  context "#get_cell" do
+    context "#set_cell_mark" do
 
-    it "returns the cell at a given [x, y] coordinates" do
-      test_config = [[Test_cell.new(:X),nil,nil], [nil,nil,nil], [nil,nil,nil]]
-      grid = Grid.new({setup: test_config})
-      expect(grid.get_cell(0, 0).value).to eq(:X)
+      it "sets the value of a cell at given [x, y] coordinates" do
+        grid.set_cell_mark(0, 0, :X)
+        expect(grid.get_cell(0, 0).value).to eq(:X)
+      end
+    end
+
+    context "#game_over" do
+
+      let(:x_cell) { Test_cell.new(:X) }
+      let(:o_cell) { Test_cell.new(:O) }
+      let(:empty_cell) { Test_cell.new }
+
+      it "returns :winner if winner? is true" do
+        allow(grid).to receive(:winner?) {true}
+        expect(grid.game_over).to eq(:winner)
+      end
+
+      it "returns :draw if draw? is true" do
+        allow(grid).to receive(:draw?) {true}
+        expect(grid.game_over).to eq(:draw)
+      end
+
+      it "returns :winner when row is complete with same marks" do
+        test_config = [[x_cell,x_cell, x_cell],
+                       [empty_cell, o_cell, o_cell],
+                       [o_cell, o_cell, x_cell]]
+        grid = Grid.new(setup: test_config)
+        expect(grid.game_over).to eq(:winner)
+      end
+
+      it "returns :winner when column is complete with same marks" do
+        test_config = [[x_cell,empty_cell, o_cell],
+                       [x_cell, o_cell, o_cell],
+                       [x_cell, o_cell, x_cell]]
+        grid = Grid.new(setup: test_config)
+        expect(grid.game_over).to eq(:winner)
+      end
+
+      it "returns :winner when left diaganol is complete with same marks" do
+        test_config = [[x_cell,empty_cell, o_cell],
+                       [o_cell, o_cell, x_cell],
+                       [o_cell, x_cell, x_cell]]
+        grid = Grid.new(setup: test_config)
+        expect(grid.game_over).to eq(:winner)
+      end
+
+      it "returns :winner when left diaganol is complete with same marks" do
+        test_config = [[x_cell,empty_cell, o_cell],
+                       [o_cell, x_cell, x_cell],
+                       [x_cell, o_cell, x_cell]]
+        grid = Grid.new(setup: test_config)
+        expect(grid.game_over).to eq(:winner)
+      end
+
+      it "returns :draw when grid is full but no winner" do
+        test_config = [[x_cell,x_cell, o_cell],
+                       [o_cell, o_cell, x_cell],
+                       [x_cell, o_cell, x_cell]]
+        grid = Grid.new(setup: test_config)
+        expect(grid.game_over).to eq(:draw)
+      end
     end
   end
-
-  context "#set_cell_mark" do
-
-    it "sets the value of a cell at given [x, y] coordinates" do
-      grid.set_cell_mark(0, 0, :X)
-      expect(grid.get_cell(0, 0).value).to eq(:X)
-    end
-  end
-
-  context "#game_over" do
-
-    let(:x_cell) { Test_cell.new(:X) }
-    let(:o_cell) { Test_cell.new(:O) }
-    let(:empty_cell) { Test_cell.new }
-
-    it "returns :winner if winner? is true" do
-      allow(grid).to receive(:winner?) {true}
-      expect(grid.game_over).to eq(:winner)
-    end
-
-    it "returns :draw if draw? is true" do
-      allow(grid).to receive(:draw?) {true}
-      expect(grid.game_over).to eq(:draw)
-    end
-
-    it "returns :winner when row is complete with same marks" do
-      test_config = [[x_cell,x_cell, x_cell],
-                     [empty_cell, o_cell, o_cell],
-                     [o_cell, o_cell, x_cell]]
-      grid = Grid.new(setup: test_config)
-      expect(grid.game_over).to eq(:winner)
-    end
-
-    it "returns :winner when column is complete with same marks" do
-      test_config = [[x_cell,empty_cell, o_cell],
-                     [x_cell, o_cell, o_cell],
-                     [x_cell, o_cell, x_cell]]
-      grid = Grid.new(setup: test_config)
-      expect(grid.game_over).to eq(:winner)
-    end
-
-    it "returns :winner when left diaganol is complete with same marks" do
-      test_config = [[x_cell,empty_cell, o_cell],
-                     [o_cell, o_cell, x_cell],
-                     [o_cell, x_cell, x_cell]]
-      grid = Grid.new(setup: test_config)
-      expect(grid.game_over).to eq(:winner)
-    end
-
-    it "returns :winner when left diaganol is complete with same marks" do
-      test_config = [[x_cell,empty_cell, o_cell],
-                     [o_cell, x_cell, x_cell],
-                     [x_cell, o_cell, x_cell]]
-      grid = Grid.new(setup: test_config)
-      expect(grid.game_over).to eq(:winner)
-    end
-
-    it "returns :draw when grid is full but no winner" do
-      test_config = [[x_cell,x_cell, o_cell],
-                     [o_cell, o_cell, x_cell],
-                     [x_cell, o_cell, x_cell]]
-      grid = Grid.new(setup: test_config)
-      expect(grid.game_over).to eq(:draw)
-    end
-  end
-
-
-
-
-
-
-end
 end
